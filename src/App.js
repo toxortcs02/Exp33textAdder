9d7d89ad68076473c8f9cebe0991d3aa96758d4a
Tomas Sabella <tomassabella@hotmail.com>
2025-07-31 17:00:59 -0300

    new world
    new world

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola mundo!
-          
+          Hola Mundo!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
