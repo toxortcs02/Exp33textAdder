55648c32d2e2ddc4933b60400b5e75352012ee0d
Tomas Sabella <tomassabella@hotmail.com>
2025-07-31 23:52:19 -0300

    Refactor App component to manage input states and update UI elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [linea1, setlinea1] = useState('');
+  const [linea2, setlinea2] = useState('');
+  const onChangel1 = function (TEXTO) {
+    setlinea1(TEXTO.target.value);
+  };
+  const onChangel2 = function (e) {
+    setlinea2(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo!
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <select>1
+        <option value="maelle">Maelle</option>
+        <option value="Sciel">Sciel</option>
+        <option value="Lune">Lune</option>
+        <option value="gustave">Gustave</option>
+        <option value="verso">Verso</option>
+        <option value="monoco">Monoco</option>
+        <option value="reno">Renoir</option>
+        <option value="Clea">Clea</option>
+      </select>  <br />
+      <input onChange={onChangel1} type="text" placeholder="linea1"/>
+      <br />
+      <input onChange={onChangel2} type="text" placeholder="linea2"/>
+      {/* boton exportar */}
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1} </span>
+        <span>{linea2}</span>
+        <img src="" />  
+      </div>
+
     </div>
   );
 }
